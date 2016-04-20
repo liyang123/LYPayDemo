fcadaee44582cb4d289aa18291a4d1605d187cd3
liyang <995720636@qq.com>
2016-04-20 14:11:59 +0800

    Initial Commit

---
+++ LYPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYPayDemo
+//
+//  Created by liyang on 16/4/20.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
